18ac55e10bddc7f234982b75b6e4d25d581b7f48
Rustam Salimov <rustam2027@yandex.ru>
2024-03-31 11:17:13 +0700

    Fix test

--- parser/Parser.hpp
+++ parser/Parser.hpp
@@ -21,30 +21,32 @@ private:
   static map<string, Register> registers_names;
   map<std::string, int> labels;
 
-  map<string, function<Command *()>> func = {{"add", []() { return new Add(); }},
-                                             {"addi", []() { return new Addi(); }},
-                                             {"and", []() { return new And(); }},
-                                             {"li", []() { return new Li(); }},
-                                             {"mv", []() { return new Mv(); }},
-                                             {"or", []() { return new Or(); }},
-                                             {"sll", []() { return new SLL(); }},
-                                             {"srl", []() { return new SRL(); }},
-                                             {"sub", []() { return new Sub(); }},
-                                             {"xor", []() { return new Xor(); }},
-                                             {"ecall", []() { return new Ecall(); }},
-                                             {"jmp", []() {return new Jump(); }}};
+  map<string, function<Command *()>> func = {
+      {"add", []() { return new Add(); }},
+      {"addi", []() { return new Addi(); }},
+      {"and", []() { return new And(); }},
+      {"li", []() { return new Li(); }},
+      {"mv", []() { return new Mv(); }},
+      {"or", []() { return new Or(); }},
+      {"sll", []() { return new SLL(); }},
+      {"srl", []() { return new SRL(); }},
+      {"sub", []() { return new Sub(); }},
+      {"xor", []() { return new Xor(); }},
+      {"ecall", []() { return new Ecall(); }},
+      {"jmp", []() { return new Jump(); }},
+      {"jal", []() { return new JumpAndLink(); }}};
 
   string file;
   vector<string> split(const string &s, char del, bool remove_comma);
   string concat(const string &sep, const vector<string> &strs);
-  void delete_commands(vector<Command*> commands);
+  void delete_commands(vector<Command *> commands);
 
 public:
   Parser(string file);
   void preprocess();
   vector<Command *> get_commands();
   static Register get_register(const string &str);
-  Command* get_command(const string &str);
+  Command *get_command(const string &str);
   map<std::string, int> get_labels() { return labels; }
 
   static string exception_message(const string &command, int required,
