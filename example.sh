239cc169add3ab5b4c4cc9a7f9dfc13449ee79d3
Luba Kolbasova <luba1607@gmail.com>
2024-04-22 19:01:14 +0700

    change comments in in.txt

--- example.sh
+++ example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clang++ main.cpp frontend/Parser.cpp frontend/Preprocessor.cpp instructions/instructions_impl.cpp interpreter/Interpreter.cpp tests/simple_instructions_test.cpp -o main -fsanitize=address -fsanitize=undefined -std=c++17 
+clang++ main.cpp frontend/Parser.cpp frontend/Lexer.cpp frontend/Preprocessor.cpp instructions/instructions_impl.cpp interpreter/Interpreter.cpp tests/simple_instructions_test.cpp -o main -fsanitize=address -fsanitize=undefined -std=c++17 
 
 ./main in.txt
 
