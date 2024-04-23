32ccfa9c4a8d3e291b1002fb6db24eba499ec9db
Luba Kolbasova <luba1607@gmail.com>
2024-04-24 00:28:27 +0700

    labels in macros

--- frontend/Preprocessor.hpp
+++ frontend/Preprocessor.hpp
@@ -18,8 +18,10 @@ class Preprocessor {
     void close_resources();
 
     struct Macros {
+      int instances = 0;
       std::vector<std::string> params;
       std::vector<std::string> macros_lines;
+      std::vector<std::string> macros_labels;
     };
 
     std::map<std::string, int> labels;
@@ -31,9 +33,13 @@ class Preprocessor {
 
     void string_replace(std::string& input, const std::string& src, const std::string& dst);
     void delete_commas(std::vector<std::string>& line);
-    void replace_eqv(std::string& str);
+    void replace(std::string& str, std::map<std::string, std::string> replace_map);
 
     std::vector<std::string> split_and_delete_comments(const std::string& s);
+    bool is_label(std::string& token);
+    void add_label(std::string& label, int lines_counter);
+    void inline_macros(std::vector<std::string>& input_line, int& counter, bool write_to_file, Macros* m_data);
+    std::map<std::string, std::string> create_replace_labels(std::vector<std::string>& macro_labels, std::string num, std::string name);
 
   public:
     Preprocessor(bool debug, std::string file): debug_mode(debug), file(file)  {}
