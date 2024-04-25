a960c3d00b6db6210be4f0ac1aed6d07d0e4f90a
Rustam Salimov <rustam2027@yandex.ru>
2024-04-25 21:07:32 +0700

    Update debugger

--- break_controller/BreakController.cpp
+++ break_controller/BreakController.cpp
@@ -5,6 +5,8 @@
 #include <cstddef>
 #include <ios>
 #include <iostream>
+#include <sstream>
+#include <stack>
 #include <vector>
 
 void BreakController::open_interface() {
@@ -15,21 +17,23 @@ void BreakController::open_interface() {
 
     if (request == "continue") {
       break;
-    } else if (request == "exit") {
+    } else if (request == "exit" || request == "q") {
       exit = true;
       break;
     } else if (request.rfind("show", 0) == 0) {
-      if (request == "show stack") {
+      if (request.rfind("show stack", 0) == 0) {
         int from, to;
-        std::cin >> from >> to;
+        std::string buffer;
+        std::stringstream stream_request(request);
+        stream_request >> buffer >> buffer >> from >> to;  # FIXME: Some how check that exactly two numbers were given
         show_stack(from, to);
       } else if (request == "show registers") {
         show_registers();
       } else {
-        std::cout << "UNKNOWN COMMAND" << std::endl;
+        std::cout << "UNKNOWN COMMAND : '" << request << "'" << std::endl;
       }
     } else {
-      std::cout << "UNKNOWN COMMAND" << std::endl;
+      std::cout << "UNKNOWN COMMAND : '" << request << "'" << std::endl;
     }
   }
 
@@ -38,18 +42,29 @@ void BreakController::open_interface() {
 
 void BreakController::show_stack(size_t from, size_t to) {
   std::cout << "SHOWING STACK" << std::endl;
+  for (int i = from; i < to; i += 4) {
+    std::cout << "[" << i << "]: ";
+    long word = 0;
+    for (int j = 0; j < 4; j++) {
+      word *= 256;
+      word += (int) global_state->stack[i + j];
+    }
+    std::cout << std::hex << word << std::endl;
+  }
 }
 
 void BreakController::show_registers() {
   std::cout << "SHOWING REGISTERS" << std::endl;
-  for (auto iter=Parser::registers_names.begin(); iter!=Parser::registers_names.end(); ++iter) {
-    std::cout << iter->first << ": " << std::hex << global_state->registers[iter->second] << std::endl;
+  for (auto iter = Parser::registers_names.begin();
+       iter != Parser::registers_names.end(); ++iter) {
+    std::cout << iter->first << ": " << std::hex
+              << global_state->registers[iter->second] << std::endl;
   }
-
 }
 
 BreakController::BreakController(std::vector<Instruction *> instructions,
-                                 std::map<std::string, int> labels): exit(false) {
+                                 std::map<std::string, int> labels)
+    : exit(false) {
   instructions_ = instructions;
   global_state = new State(labels);
 }
