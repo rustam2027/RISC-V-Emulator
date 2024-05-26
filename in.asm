f32fbe366d8a72814c411d809698833ba18439f6
Luba Kolbasova <luba1607@gmail.com>
2024-05-27 01:07:12 +0700

    improve example

--- in.asm
+++ in.asm
@@ -1,8 +1,10 @@
 .section .data
 hello:
-  .string "Hello world!"
+  .string "Hello world! "
 
-.section .text
+number:
+  .word 42
+
 
 .macro print_char %src
   mv a0, %src
@@ -10,32 +12,57 @@ hello:
   ecall
 .end_macro
 
-li a6, -8
+.macro print_int %src
+  mv a0, %src
+  li a7, 1
+  ecall
+.end_macro
 
-loop_2:
-  ebreak
-  addi a6, a6, 8
+.macro exit %ecode
+  li a0, 0
+  li a7, 93  
+  ecall
+.end_macro
 
-main:
+
+.eqv word_size 8      # 8 bytes in word
+.eqv byte_size 8      # 8 bit in byte
+.eqv char_mask 0xFF
+
+.section .text
+
+
+#-------------------------------------------#
+# a6 - words counter                        #
+# a5 - inner counter for shift              #
+# starts from max 56 (64 - 8) and decreases #
+# in step of byte_size (8) to 0             #
+#-------------------------------------------#
+
+
+li a6, -word_size
+li a3, char_mask
+
+loop:
+  addi a6, a6, word_size      
   la a2, hello
   add a2, a2, a6
   lw a1, 0(a2)
   li a5, 56
 
-loop:
-  li a3, 0xFF
-  sll a3, a3, a5
-  and a0, a1, a3
-  srl a0, a0, a5
-  beqz a0, end
-  print_char a0
-  addi a5, a5, -8
-  blt a5, zero, loop_2
-  j loop
+  get_char:
+    srl a4, a1, a5
+    and a0, a4, a3
+    beqz a0, get_number
+    print_char a0
+    addi a5, a5, -byte_size
+    blt a5, zero, loop
+    j get_char
 
-end:
+get_number:
+  la a2, number
+  lw a1, 0(a2)
+  print_int a1
 
-  
-
-  
+exit 0
 
